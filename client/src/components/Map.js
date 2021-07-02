5530329955fc370aea0b3fcd51bfbb3d3b6a6c08
Spencer Haugh <spencer.haugh@gmail.com>
2021-07-02 11:24:14 -0400

    Map connected and displaying

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,31 @@
 import React from "react";
+import ReactMapGL from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const viewport = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1Ijoic3BlbmNlcmhhdWdoIiwiYSI6ImNrcWw4cnVuMjAybm0ybnE0dGR1dnd2eXAifQ.00jDcQastThwnM6QKzy7OQ"
+        {...viewport}
+      >
+
+      </ReactMapGL>
+
+    </div>
+  )
 };
 
 const styles = {
